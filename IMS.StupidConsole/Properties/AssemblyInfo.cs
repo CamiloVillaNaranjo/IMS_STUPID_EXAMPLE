31a867e9054da0b3d12a766fa68739822cb29922
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-01-29 13:04:54 -0500

    Creating Branch with TightCoupling example

--- IMS.StupidConsole/Properties/AssemblyInfo.cs
+++ IMS.StupidConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IMS.StupidConsole")]
+[assembly: AssemblyTitle("Yuxi.StupidConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IMS.StupidConsole")]
+[assembly: AssemblyProduct("Yuxi.StupidConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
---
+++ IMS.StupidLibrary/Computer.cs
@@ -0,0 +1,9 @@
+﻿namespace Yuxi.StupidLibrary
+{
+    public class Computer
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Series { get; set; }
+    }
+}
