56c3cfa1a70c8cc1394e61bf66e164024d71b42a
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-01-29 13:29:21 -0500

    Rearranging master branch

--- IMS.StupidLibrary/Computer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Yuxi.StupidLibrary
-{
-    public class Computer
-    {
-        public string Brand { get; set; }
-        public string Model { get; set; }
-        public string Series { get; set; }
-    }
-}
