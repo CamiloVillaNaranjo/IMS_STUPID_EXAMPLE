4822b445ef3189f6a267a226a2514751e66065b2
cvillanaranjo <camilo.villa@yuxiglobal.com>
2017-01-29 11:18:05 -0500

    Branch with Singleton Issues related.

--- IMS.StupidConsole/Program.cs
+++ IMS.StupidConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IMS.StupidLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,22 @@ namespace IMS.StupidConsole
     {
         static void Main(string[] args)
         {
+            var myTv = Television.Instance;
+            myTv.ColorMode = "CMYK";
+
+            var myTvNew = Television.Instance;
+            myTvNew.ColorMode = "RGB";
+
+
+            if(myTv == myTvNew)
+            {
+                Console.WriteLine("instance 'myTv' and 'myTvNew' are equals");
+                Console.WriteLine("Because its property values for ColoMode are:");
+                Console.WriteLine("'myTv' - '{0}'", myTv.ColorMode);
+                Console.WriteLine("'myTvNew' - '{0}'", myTvNew.ColorMode);
+
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ IMS.StupidLibrary/Television.cs
@@ -0,0 +1,37 @@
+﻿namespace IMS.StupidLibrary
+{
+    public class Television
+    {
+        private static Television tv = null;
+        private static readonly object padlock = new object();
+
+        Television() { }
+
+        public static Television Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (tv==null)
+                    {
+                        tv = new Television();
+                    }
+                    return tv;
+                }
+            }
+        }
+
+        public static string PowerOn()
+        {
+            return "Television Powered On!";
+        }
+
+        private string _colorMode;
+        public string ColorMode
+        {
+            get { return _colorMode; }
+            set { _colorMode = value; }
+        }
+    }
+}
